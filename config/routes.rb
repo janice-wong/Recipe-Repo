74193a3ad9a178c93b5580e1376e1427c3717d07
Janice Wong <go.janicewong@gmail.com>
2017-10-05 17:02:40 -0400

    Update recipe and ingredient controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :ingredients
-  resources :recipes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'recipes#index'
+  resources :recipes do
+    resources :ingredients
+  end
 end
